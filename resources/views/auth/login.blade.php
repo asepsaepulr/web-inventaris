eb7b95ceb72d4340e8d54f92c3a6dba546093170
adrnsyhrvndra <adrimedia.indonesia@gmail.com>
2023-01-04 13:53:22 +0700

    Merapihkan Halaman Login Part 02

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -73,59 +73,65 @@
 
           @csrf
 
-          <div class="form-group row">
+          <div class="row">
 
-            <label for="email" class=" col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+              <div class="col-lg-12 mt-1">
 
-            <div class="">
+                <div class="form-group">
 
-              <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <label for="email" class="form-label">{{ __('E-Mail Address') }}</label>
 
-              @error('email')
-
-                <span class="invalid-feedback" role="alert">
-
-                  <strong>{{ $message }}</strong>
-
-                </span>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" value="{{ old('email') }}" aria-describedby="email" required autocomplete="email" autofocus>
                 
-              @enderror
+                    @error('email')
 
-            </div>
+                      <span class="invalid-feedback" role="alert">
 
-            <div class="form-group row">
-              <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <strong>{{ $message }}</strong>
 
-              <div>
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                      </span>
+                      
+                    @enderror
 
-                @error('password')
-                  <span class="invalid-feedback" role="alert">
-                  <strong>{{ $message }}</strong>
-                  </span>
-                @enderror
+                  </div>
+
+              </div>
+
+              <div class="col-lg-12 mt-1">
+
+                <div class="form-group">
+
+                    <label for="password" class="form-label">{{ __('Password') }}</label>
+
+                    <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" aria-describedby="password" required autocomplete="current-password">
+
+                    @error('password')
+
+                      <span class="invalid-feedback" role="alert">
+
+                        <strong>{{ $message }}</strong>
+                        
+                      </span>
+
+                    @enderror
+
                 </div>
-            </div>
 
-            <div class="form-group row mt-3">
+              </div>
 
-              <div class="col-lg-12 d-flex justify-content-between">
+              <div class="col-lg-12 mt-3 d-flex justify-content-between">
 
-                <div class="form-check">
+                <div class="form-check mb-3">
 
-                  <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <input type="checkbox" class="form-check-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                  <label class="form-check-label" for="remember">
-
-                    {{ __('Remember Me') }}
-
-                  </label>
+                    <label class="form-check-label" for="remember">{{ __('Remember Me') }}</label>
 
                 </div>
 
                 @if (Route::has('password.request'))
 
-                  <a class="float-right" href="{{ route('password.request') }}">
+                  <a href="{{ route('password.request') }}">
 
                       {{ __('Forgot Your Password?') }}
 
@@ -135,21 +141,15 @@
 
               </div>
 
-            </div>
+              <div class="col-lg-12 d-flex justify-content-between" >
 
-            <div class="form-group row mt-4 mb-0">
-
-              <div class="col-md-8 offset-md-4">
-
-                <button type="submit" class="btn btn-primary">
+                <button type="submit" class="btn btn-primary" style="width:100%">
 
                   {{ __('Sign In') }}
 
                 </button>
 
               </div>
-
-            </div>
 
           </div>
 
