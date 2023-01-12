0d6ad88412dd2618695f75dd378d443f7fb1efd2
Adriansyah Ravindra <adrimedia.indonesia@gmail.com>
2023-01-12 20:56:39 +0700

    Merapihkan Semua Design Alert

--- resources/views/room/index.blade.php
+++ resources/views/room/index.blade.php
@@ -1,6 +1,47 @@
-@extends('layouts.app') @section('content') @if ($message = Session::get('succes')) <div class="alert alert-success">
-  <p>{{ $message }}</p>
-</div> @endif <div class="row">
+@extends('layouts.app') @section('content') 
+
+@if ($message = Session::get('succes')) 
+
+  <div class="row">
+
+    <div class="col-lg-12">
+
+      <div class="bd-example">
+
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+
+          <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+          
+          </symbol>
+
+        </svg>
+
+        <div class="alert alert-solid alert-success rounded-0 alert-dismissible fade show " role="alert">
+
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24">
+
+                <use xlink:href="#check-circle-fill" />
+
+            </svg>
+
+            <span>{{ $message }}</span>
+
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+@endif 
+
+
+<div class="row">
   <div class="col-sm-12">
     <div class="card">
       <div class="card-body">
--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -2,6 +2,76 @@
 
 @section('content')
 
+   @if ($message = Session::get('error'))
+
+      <div class="row">
+
+         <div class="col-lg-12">
+
+            <div class="bd-example">
+
+            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+
+               <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+
+                  <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+               
+               </symbol>
+
+            </svg>
+
+            <div class="alert alert-solid alert-danger rounded-0 alert-dismissible fade show " role="alert">
+
+                  <svg class="bi flex-shrink-0 me-2" width="24" height="24">
+
+                     <use xlink:href="#exclamation-triangle-fill" />
+
+                  </svg>
+
+                  <span>{{ $message }}</span>
+
+                  <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
+
+            </div>
+
+            </div>
+
+         </div>
+
+      </div>
+
+   @elseif ($errors->any())
+
+      <div class="row">
+
+         <div class="col-lg-12">
+
+            <div class="bd-example">
+
+            <div class="alert alert-solid alert-danger rounded-0 alert-dismissible fade show " role="alert">
+
+               <ul>
+
+                  @foreach ($errors->all() as $error)
+
+                     <li>{{ $error }}</li>
+
+                  @endforeach
+
+               </ul>
+
+               <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
+
+            </div>
+
+            </div>
+
+         </div>
+
+      </div>
+
+   @endif
+
    <div class="row ">
 
       <div class="col-sm-12">
@@ -23,7 +93,7 @@
                            </li>
 
                            <li class="breadcrumb-item" aria-current="page">
-                            
+                           
                               <a class="" href="{{ route('user.index') }}"> Home Page Users </a>
                            
                            </li>
